404810bded079ba52f40bb846b235aca154854c6
MohsinRiaz363 <mohsinriaz363@gmail.com>
2026-07-05 09:04:05 +0500

    add the missing button

---
+++ app/ui/invoices/buttons.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
+
+export function CreateInvoice() {
+  return (
+    <Link
+      href="/dashboard/invoices/create"
+      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white"
+    >
+      <span className="hidden md:block">Create Invoice</span>
+      <PlusIcon className="h-5 md:ml-4" />
+    </Link>
+  );
+}
+
+export function UpdateInvoice({ id }: { id: string }) {
+  return (
+    <Link
+      href={`/dashboard/invoices/${id}/edit`}
+      className="rounded-md border p-2 hover:bg-gray-100"
+    >
+      <PencilIcon className="w-5" />
+    </Link>
+  );
+}
+
+export function DeleteInvoice({ id }: { id: string }) {
+  return (
+    <button className="rounded-md border p-2 hover:bg-gray-100">
+      <span className="sr-only">Delete {id}</span>
+      <TrashIcon className="w-5" />
+    </button>
+  );
+}
